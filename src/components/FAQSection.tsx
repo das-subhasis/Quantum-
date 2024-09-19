8063792f19b8c61910ed4d50127835b0af52583a
Subhasis Das <das.shubasis2003@gmail.com>
2024-09-19 22:36:02 +0530

    Fixed Type validation bugs

--- src/components/FAQSection.tsx
+++ src/components/FAQSection.tsx
@@ -1,8 +1,30 @@
 import { useState } from "react";
-import { MdKeyboardArrowDown } from "react-icons/md";
-import { MdKeyboardArrowUp } from "react-icons/md";
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
 
-const FAQSection = () => {
+interface FAQProps {
+  Question: string;
+  Answer: string;
+}
+
+const FAQ: React.FC<FAQProps> = ({ Question, Answer }) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  return (
+    <div className="bg-white rounded-2xl w-full px-5">
+      <div className="flex items-center justify-between w-full p-4">
+        <p className="">{Question}</p>
+        <button onClick={() => setIsOpen(!isOpen)} className="text-left transition-all duration-300 ease-out">
+          {isOpen ? <MdKeyboardArrowUp /> : <MdKeyboardArrowDown />}
+        </button>
+      </div>
+      <div className={`transition-all duration-300 ease-out ${isOpen ? 'max-h-screen' : 'max-h-0 overflow-hidden'}`}>
+        <p className="p-4 text-gray-700">{Answer}</p>
+      </div>
+    </div>
+  );
+};
+
+const FAQSection: React.FC = () => {
   return (
     <div className="flex flex-col gap-5">
       <div>
@@ -11,27 +33,8 @@ const FAQSection = () => {
       <FAQ Question={'How long does it take to learn?'} Answer={'It takes around 2 to 3 weeks.'} />
       <FAQ Question={'How long does it take to learn?'} Answer={'It takes around 2 to 3 weeks.'} />
       <FAQ Question={'How long does it take to learn?'} Answer={'It takes around 2 to 3 weeks.'} />
-    </div >
+    </div>
   );
 };
-
-const FAQ = ({ Question, Answer }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  return (
-    <div className="bg-white rounded-2xl w-full px-5">
-      <div className="flex items-center justify-between w-full p-4">
-        <p className="">{Question}</p>
-        <button onClick={() => setIsOpen(!isOpen)} className="text-left transition-all duration-300 ease-out">
-          {
-            isOpen ? <MdKeyboardArrowUp /> : <MdKeyboardArrowDown />
-          }
-        </button>
-      </div>
-      <div className={`transition-all duration-300 ease-out ${isOpen ? 'max-h-screen' : 'max-h-0 overflow-hidden'}`}>
-        <p className="p-4 text-gray-700">{Answer}</p>
-      </div>
-    </div>
-  )
-}
 
 export default FAQSection;
